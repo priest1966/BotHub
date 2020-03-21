cf0a54449f947833f7f38f703b927c50c5ba9790
priest1966 <53721478+priest1966@users.noreply.github.com>
2020-03-21 22:08:12 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -14,7 +14,7 @@ async def _(event):
         s_helpme_string = borg._plugins[splugin_name].__doc__
     else:
         s_helpme_string = "****:"
-    helpme_string = """@Bot_Hub_Official™️ ( **Custom Built By** @Three_Cube_TeKnoways_bot ) \n**Verified Account**: ✅\n**Official \n**NOTICE**: **COMMANDS** are CASE **sensitive**\n**DESCRIPTION**: https://telegra.ph/command-list-for-BotHub-Userbot-11-08\n
+    helpme_string = """@Bot_Hub_Official™️ ( **Custom Built By** @priest1966 ) \n**Verified Account**: ✅\n**Official \n**NOTICE**: **COMMANDS** are CASE **sensitive**\n
  """.format(
         sys.version,
         __version__
@@ -61,7 +61,7 @@ async def _(event):
     plugin_name = event.pattern_match.group(1)
     if plugin_name in borg._plugins:
         helpme_string = borg._plugins[plugin_name].__doc__
-        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @Three_Cube_TeKnoways_Bot"
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @priest1966"
         if helpme_string:
             plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{helpme_string}\n{unload_string}"
         else:
