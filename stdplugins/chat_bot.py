deb1e5f72290f34cf8c4f0efb0a6151824771061
priest1966 <53721478+priest1966@users.noreply.github.com>
2020-03-21 22:33:07 +0530

    Update chat_bot.py

--- stdplugins/chat_bot.py
+++ stdplugins/chat_bot.py
@@ -32,6 +32,6 @@ async def chat_bot(event):
 		msg = str(text.message)
 		reply = bot.get_response(msg)
 		print(reply)
-		await event.edit("**Three_Cube_TeKnoways_Bot:\n"+str(reply)+"**")
+		await event.edit("**Priest:\n"+str(reply)+"**")
 	else:
 		await event.edit("Hold On I am still Training Myself...")    
